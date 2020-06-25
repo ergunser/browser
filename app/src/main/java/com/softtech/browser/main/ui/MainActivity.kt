b2c77d003da91836da72e655b9bb9775ab250be9
erguns <serkan.ergun@commencis.com>
2020-06-26 02:42:15 +0300

    List: Connect view model to activity for test purpose

--- app/src/main/java/com/softtech/browser/main/ui/MainActivity.kt
+++ app/src/main/java/com/softtech/browser/main/ui/MainActivity.kt
@@ -1,13 +1,19 @@
-package com.softtech.browser.main
+package com.softtech.browser.main.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProviders
 import com.softtech.browser.R
+import com.softtech.browser.main.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java) // FIXME change this
     }
 }
