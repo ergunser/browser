884ffcba9be71b5a43a4dc4ff90e8d66ed5810db
erguns <serkan.ergun@commencis.com>
2020-06-26 17:59:13 +0300

    Refactor request to add dispose bag

--- app/src/main/java/com/softtech/browser/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/softtech/browser/main/viewmodel/MainViewModel.kt
@@ -3,7 +3,6 @@ package com.softtech.browser.main.viewmodel
 import com.softtech.browser.base.viewmodel.BaseViewModel
 import com.softtech.browser.main.network.Api
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
@@ -12,22 +11,20 @@ class MainViewModel : BaseViewModel() {
     @Inject
     lateinit var api: Api
 
-    private lateinit var subscription: Disposable
-
     init {
         fetchBrowsers()
     }
 
     private fun fetchBrowsers() {
 
-        subscription = api.fetchBrowsers()
+        api.fetchBrowsers()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
                 //FIXME handle success
             }, {
                 //FIXME handle error
-            })
+            }).autoDispose()
 
     }
 
