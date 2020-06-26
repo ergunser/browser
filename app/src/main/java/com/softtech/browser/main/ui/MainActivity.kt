030924117e7c6aea2e76f03c683f0760f34aee07
erguns <serkan.ergun@commencis.com>
2020-06-26 18:44:21 +0300

    List:Update main activity layout for test purpose

--- app/src/main/java/com/softtech/browser/main/ui/MainActivity.kt
+++ app/src/main/java/com/softtech/browser/main/ui/MainActivity.kt
@@ -2,18 +2,26 @@ package com.softtech.browser.main.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.softtech.browser.R
+import com.softtech.browser.databinding.ActivityMainBinding
 import com.softtech.browser.main.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java) // FIXME change this
+
+        binding.viewModel = viewModel
     }
 }
